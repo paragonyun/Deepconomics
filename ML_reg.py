016f8f72ec491bf6273b17d1699a28e6fda1c37c
paragonyun <paragonyun@naver.com>
2022-12-29 20:45:44 +0900

    ⬛ Clean Codes

--- ML_reg.py
+++ ML_reg.py
@@ -1,28 +1,30 @@
-from xgboost import XGBRegressor 
+from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
 import matplotlib.pyplot as plt
 
 from xgboost import plot_importance
 from lightgbm import plot_importance
-# from shap import *
-from eli5.sklearn import permutation_importance 
 
-class xgb :
-    '''
+# from shap import *
+from eli5.sklearn import permutation_importance
+
+
+class xgb:
+    """
     XGB 모델을 돌리기 전 데이터는 정상성이 확보 되어 있는 상태여야 합니다!
-    '''
-    def __init__(self, X_train, y_train , X_test, y_test) :
+    """
+
+    def __init__(self, X_train, y_train, X_test, y_test):
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
 
-
-    def train(self, Regressor='x') :
-        if Regressor == 'x' :
+    def train(self, Regressor="x"):
+        if Regressor == "x":
             model = XGBRegressor()
 
-        elif Regressor == 'l' :
+        elif Regressor == "l":
             model = LGBMRegressor()
 
         model.fit(self.X_train, self.y_train)
@@ -31,36 +33,17 @@ class xgb :
 
         return preds, model
 
-    def plot_preds(self, preds) :
-        
-        plt.plot(self.y_test, label = 'Actual')
-        plt.plot(preds, label = 'Prediction')
+    def plot_preds(self, preds):
+
+        plt.plot(self.y_test, label="Actual")
+        plt.plot(preds, label="Prediction")
         plt.xticks(rotation=45)
         plt.legend()
         plt.show()
 
-    def check_importances(self, model) :
-        fig, axes = plt.subplots(1,1, figsize=(10,25))
+    def check_importances(self, model):
+        fig, axes = plt.subplots(1, 1, figsize=(10, 25))
 
         plot_importance(model, ax=axes[0])
-        
+
         plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- VAR_reg.py
+++ VAR_reg.py
@@ -8,30 +8,30 @@ from statsmodels.tsa.api import VAR
 from statsmodels.tsa.stattools import adfuller, grangercausalitytests
 
 
-class VARModel :
-    def __init__(self, df : pd.DataFrame) :
+class VARModel:
+    def __init__(self, df: pd.DataFrame):
         self.df = df
 
-    def find_p(self) :
+    def find_p(self):
         model = VAR(self.df)
 
         results = []
-        for i in range(1, 20) :
+        for i in range(1, 20):
             result = model.fit(i)
             results.append(result.aic)
 
         plt.plot(list(range(len(results))), results)
-        plt.title('AIC Scores')
-        plt.xlabel('Order')
-        plt.ylabel('AIC')
+        plt.title("AIC Scores")
+        plt.xlabel("Order")
+        plt.ylabel("AIC")
         plt.show()
 
-        print(f'최소 AIC 값 : p = {min(results)}')
-        print(f'최적의 p 값 : {results.index(min(results))+1}')
+        print(f"최소 AIC 값 : p = {min(results)}")
+        print(f"최적의 p 값 : {results.index(min(results))+1}")
 
-        return results.index(min(results))+1
+        return results.index(min(results)) + 1
 
-    def train(self, p) :
+    def train(self, p):
         model = VAR(self.df)
         model_fitted = model.fit(p)
 
@@ -39,40 +39,45 @@ class VARModel :
 
         return model_fitted
 
-    def forecast(self, fitted_model , train_df, test_df, min_p, step=5) :
+    def forecast(self, fitted_model, train_df, test_df, min_p, step=5):
         lagged = train_df.values[-min_p:]
         # lagged = test_df.values
-        prediction = fitted_model.forecast(y = lagged, steps= step) ## steps : X_test가 5개로 할 거니 5개로 결정...!
-        cols = [str(i) for i in range(len(train_df.columns) -1)]
-        cols.append('Target_preds')
-        df_pred = pd.DataFrame(prediction, index=test_df.index, columns =cols)
+        prediction = fitted_model.forecast(
+            y=lagged, steps=step
+        )  ## steps : X_test가 5개로 할 거니 5개로 결정...!
+        cols = [str(i) for i in range(len(train_df.columns) - 1)]
+        cols.append("Target_preds")
+        df_pred = pd.DataFrame(prediction, index=test_df.index, columns=cols)
 
         return df_pred
 
-    def grangers_causation_matrix(self, data, variables, test='ssr_chi2test'):    
-        '''
-        grangers_causation_matrix(df, variables = df.columns)  
-        '''
+    def grangers_causation_matrix(self, data, variables, test="ssr_chi2test"):
+        """
+        grangers_causation_matrix(df, variables = df.columns)
+        """
         maxlag = 14
 
-        df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
+        df = pd.DataFrame(
+            np.zeros((len(variables), len(variables))),
+            columns=variables,
+            index=variables,
+        )
         for c in df.columns:
             for r in df.index:
-                test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
-                p_values = [round(test_result[i+1][0][test][1],4) for i in range(maxlag)]
+                test_result = grangercausalitytests(
+                    data[[r, c]], maxlag=maxlag, verbose=False
+                )
+                p_values = [
+                    round(test_result[i + 1][0][test][1], 4) for i in range(maxlag)
+                ]
 
                 min_p_value = np.min(p_values)
                 df.loc[r, c] = min_p_value
-        df.columns = [var + '_x' for var in variables]
-        df.index = [var + '_y' for var in variables]
+        df.columns = [var + "_x" for var in variables]
+        df.index = [var + "_y" for var in variables]
         return df
 
-    def plot_impulse(self, fitted_model, num) :
-        # fig, ax = plt.subplots(figsize=(100,100))
-        # # ax.fitted_model.irf(num)
-
-        # fig.show()
-        # plt.figure(figsize=(500,100))
-        fitted_model.irf(num).plot(response='Target', figsize=(10,50))
+    def plot_impulse(self, fitted_model, num):
+        fitted_model.irf(num).plot(response="Target", figsize=(10, 50))
         plt.tight_layout()
         plt.show()
